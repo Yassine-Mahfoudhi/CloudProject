c2624a09c120e227119cd8d0de17ddfa16ca80b4
Mahfoudhi Yassine <96590738+Yassine-Mahfoudhi@users.noreply.github.com>
2025-12-04 22:35:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -176,5 +176,5 @@ def place_order():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=8000)
 
